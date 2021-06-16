6822689944d05e91d38f854b7c91ffb635488143
Francisco Guerra <82596122+Francisco-Guerra@users.noreply.github.com>
2021-06-15 23:02:18 -0400

    Mi primer programa Java

---
+++ 7.1 Mi Primer Programa/HolaMundo/src/MiPrimerPrograma.java
@@ -0,0 +1,17 @@
+
+public class MiPrimerPrograma {
+
+	public static void main(String[] args) {
+		
+		String nombre = "Francisco";
+		System.out.println("Mi nombre es "+ nombre);
+		
+		int edad = 31;
+		System.out.println("Tengo "+ edad +" años de edad.");
+		
+		String ciudad = "Temuco";
+		System.out.println("Mi ciudad es "+ ciudad);
+		
+	}
+
+}
