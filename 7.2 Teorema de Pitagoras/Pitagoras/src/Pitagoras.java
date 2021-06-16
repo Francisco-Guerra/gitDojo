a23794bec00365350b00f89aa92310ba6e5a313e
Francisco Guerra <82596122+Francisco-Guerra@users.noreply.github.com>
2021-06-16 13:17:54 -0400

    Teorema de Pitagoras

---
+++ 7.2 Teorema de Pitagoras/Pitagoras/src/Pitagoras.java
@@ -0,0 +1,18 @@
+
+public class Pitagoras {
+	public double calcularHipotenusa(int catetoA, int catetoB) {
+    double catetoC = Math.sqrt((catetoA * catetoA) + (catetoB * catetoB));
+    return catetoC;
+		
+    }
+	
+	//ATRIBUTOS
+	double catetoA;
+	double catetoB;
+	
+	//METODOS
+	public void calcularHipotenusa() {
+		
+	}
+
+}
---
+++ 7.2 Teorema de Pitagoras/Pitagoras/src/PitagorasTest.java
@@ -0,0 +1,12 @@
+
+public class PitagorasTest {
+
+	public static void main(String[] args) {
+		
+		Pitagoras calcHipotenusa = new Pitagoras();
+		
+		System.out.println(calcHipotenusa.calcularHipotenusa(7 , 24));
+		
+	}
+
+}
