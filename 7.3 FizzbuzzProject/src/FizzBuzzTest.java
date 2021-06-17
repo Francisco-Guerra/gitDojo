61ed18723ab1350b16ee75b1646c5ee0e019bcb1
Francisco Guerra <82596122+Francisco-Guerra@users.noreply.github.com>
2021-06-17 11:23:38 -0400

    FizzBuzz

---
+++ 7.3 FizzbuzzProject/src/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+
+public class FizzBuzzTest {
+
+	public static void main(String[] args) {
+		FizzBuzz fb = new FizzBuzz();
+		FizzBuzz fb2 = new FizzBuzz();
+		FizzBuzz fb3 = new FizzBuzz();
+		FizzBuzz fb4 = new FizzBuzz();
+		System.out.println(fb.fizzBuzz(15));
+		System.out.println(fb2.fizzBuzz(6));
+		System.out.println(fb3.fizzBuzz(10));
+		System.out.println(fb4.fizzBuzz(11));
+	
+	}
+
+}
